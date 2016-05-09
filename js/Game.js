d5f24ed1407b84ccd0b49ea9cf4c658075e586b2
leandro <olegon.main@gmail.com>
2016-05-09 11:24:11 -0300

    - Adicionado versão de Game.js sem construtor.

--- js/Game.js
+++ js/Game.js
@@ -1,128 +1,126 @@
-(function() {
+var Game = (function() {
     'use strict';
 
-    function Game(canvasId) {
-        var self = this;
+    var engine = {
+        GAME_OVER: 'game_over',
+        GAME_WILL_END: 'game_will_end',
+        GAME_WAITING_TO_START: 'game_waiting_to_start',
+        GAME_RUNNING: 'game_running',
 
-        self.canvasId = canvasId;
+        coreState: {
 
-        self.coreState = {
-
-        };
-
-        self.state = {
-            'get2dContext': function() {
-                return self.coreState.canvasCtx;
+        },
+        state: {
+            get2dContext: function() {
+                return this.coreState.canvasCtx;
             },
-            'getCanvasElement': function() {
-                return self.coreState.canvasElement;
+            getCanvasElement: function() {
+                return this.coreState.canvasElement;
             },
-        };
-    }
+        },
 
-    Game.GAME_OVER = 'game_over';
-    Game.GAME_WILL_END = 'game_will_end';
-    Game.GAME_WAITING_TO_START = 'game_waiting_to_start';
-    Game.GAME_RUNNING = 'game_running';
+        init: function(initFunction) {
+            this.coreState.canvasElement = window.document.getElementById(this.canvasId);
 
-    Game.prototype.init = function(initFunction) {
-        this.coreState.canvasElement = window.document.getElementById(this.canvasId);
-
-        if (this.coreState.canvasElement == null) {
-            throw new Error('O elemento Canvas cujo id é "' + this.canvasId + '" não foi encontrado.');
-        }
-
-        this.coreState.canvasCtx = this.coreState.canvasElement.getContext('2d');
-
-        if (this.coreState.canvasCtx == null) {
-            throw new Error('Não foi possível obter o contexto 2d do elemento Canvas.');
-        }
-
-        this.state.game_status = Game.GAME_WAITING_TO_START;
-
-        if (initFunction) initFunction(this.state);
-    };
-
-    Game.prototype.start = function() {
-        var self = this;
-
-        var state = this.state;
-
-        state.keyboard = {
-            space: false,
-            left: false,
-            right: false,
-            up: false,
-            down: false
-        };
-
-        (function inputEventSetup() {
-            function preventScrolling(e) {
-                if ([37, 38, 39, 40, 32].indexOf(e.keyCode) > -1) {
-                    e.preventDefault();
-                }
+            if (this.coreState.canvasElement == null) {
+                throw new Error('O elemento Canvas cujo id é "' + this.canvasId + '" não foi encontrado.');
             }
 
-            window.addEventListener('keydown', function(e) {
-                if (e.keyCode === 37) {
-                    state.keyboard.left = true;
-                } else if (e.keyCode === 38) {
-                    state.keyboard.up = true;
-                } else if (e.keyCode === 39) {
-                    state.keyboard.right = true;
-                } else if (e.keyCode === 40) {
-                    state.keyboard.down = true;
-                } else if (e.keyCode === 32) {
-                    state.keyboard.space = true;
+            this.coreState.canvasCtx = this.coreState.canvasElement.getContext('2d');
+
+            if (this.coreState.canvasCtx == null) {
+                throw new Error('Não foi possível obter o contexto 2d do elemento Canvas.');
+            }
+
+            this.state.game_status = this.GAME_WAITING_TO_START;
+
+            if (initFunction) initFunction(this.state);
+        },
+        start: function() {
+            var self = this;
+
+            var state = this.state;
+
+            state.keyboard = {
+                space: false,
+                left: false,
+                right: false,
+                up: false,
+                down: false
+            };
+
+            (function inputEventSetup() {
+                function preventScrolling(e) {
+                    if ([37, 38, 39, 40, 32].indexOf(e.keyCode) > -1) {
+                        e.preventDefault();
+                    }
                 }
 
-                preventScrolling(e);
-            });
+                window.addEventListener('keydown', function(e) {
+                    if (e.keyCode === 37) {
+                        state.keyboard.left = true;
+                    } else if (e.keyCode === 38) {
+                        state.keyboard.up = true;
+                    } else if (e.keyCode === 39) {
+                        state.keyboard.right = true;
+                    } else if (e.keyCode === 40) {
+                        state.keyboard.down = true;
+                    } else if (e.keyCode === 32) {
+                        state.keyboard.space = true;
+                    }
 
-            window.addEventListener('keyup', function(e) {
-                if (e.keyCode === 37) {
-                    state.keyboard.left = false;
-                } else if (e.keyCode === 38) {
-                    state.keyboard.up = false;
-                } else if (e.keyCode === 39) {
-                    state.keyboard.right = false;
-                } else if (e.keyCode === 40) {
-                    state.keyboard.down = false;
-                } else if (e.keyCode === 32) {
-                    state.keyboard.space = false;
+                    preventScrolling(e);
+                });
+
+                window.addEventListener('keyup', function(e) {
+                    if (e.keyCode === 37) {
+                        state.keyboard.left = false;
+                    } else if (e.keyCode === 38) {
+                        state.keyboard.up = false;
+                    } else if (e.keyCode === 39) {
+                        state.keyboard.right = false;
+                    } else if (e.keyCode === 40) {
+                        state.keyboard.down = false;
+                    } else if (e.keyCode === 32) {
+                        state.keyboard.space = false;
+                    }
+
+                    preventScrolling(e);
+                });
+            })();
+
+            (function gameLoopSetup() {
+                var accTime = 0;
+                var previousTime = performance.now();
+
+                function gameloop(currentTime) {
+                    // delta time
+                    var dt = currentTime - previousTime;
+                    previousTime = currentTime;
+
+                    if (self.updateCallback) self.updateCallback(dt, self.state);
+                    if (self.drawCallback) self.drawCallback(dt, self.state);
+
+                    window.requestAnimationFrame(gameloop);
                 }
-
-                preventScrolling(e);
-            });
-        })();
-
-        (function gameLoopSetup() {
-            var accTime = 0;
-            var previousTime = performance.now();
-
-            function gameloop(currentTime) {
-                // delta time
-                var dt = currentTime - previousTime;
-                previousTime = currentTime;
-
-                if (self.updateCallback) self.updateCallback(dt, self.state);
-                if (self.drawCallback) self.drawCallback(dt, self.state);
 
                 window.requestAnimationFrame(gameloop);
-            }
-
-            window.requestAnimationFrame(gameloop);
-        })();
+            })();
+        },
+        setUpdateCallback: function(updateCallback) {
+            this.updateCallback = updateCallback;
+        },
+        setDrawCallback: function(drawCallback) {
+            this.drawCallback = drawCallback;
+        }
     };
 
-    Game.prototype.setUpdateCallback = function(updateCallback) {
-        this.updateCallback = updateCallback;
-    };
+    return function(canvasId) {
+        var game = Object.create(engine);
 
-    Game.prototype.setDrawCallback = function(drawCallback) {
-        this.drawCallback = drawCallback;
-    };
+        game.canvasId = canvasId;
 
-    window.Game = Game;
+        return game;
+    };
 
 })();
